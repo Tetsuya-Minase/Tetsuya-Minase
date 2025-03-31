69a7b96ae82f272309b52e76eded555fa0a39149
Tetsuya-Minase <tetsuya.minasen@gmail.com>
2025-04-01 01:22:45 +0900

    feat: add self introduction pages and remove unused templates.

---
+++ components/Icon.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+<style scoped lang="css">
+.icon {
+  position: absolute;
+  top: 10%;
+  right: 10%;
+  width: 200px;
+  height: 200px;
+  border: 1px solid #000;
+  border-radius: 50%;
+  background-color: #fff;
+}
+</style>
+<template>
+  <img class="icon" src="../images/darts_pictgram.png" alt="darts_icon" />
+</template>
